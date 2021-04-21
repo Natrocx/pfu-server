a7bd96e7698be477df4d94080fe7d35f9b868877
Jonas Lauschke <natrocx@protonmail.ch>
2021-04-21 14:05:00 +0200

    Initial commit

---
+++ src/main/java/de/dhbw_mannheim/pfu_server/PfuServerApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw_mannheim.pfu_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dhbw_mannheim/pfu_server/PfuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw_mannheim.pfu_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
