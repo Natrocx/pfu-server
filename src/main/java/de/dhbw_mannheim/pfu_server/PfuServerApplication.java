b85cd7b56950eaa18640c31f59c273bfc96506ae
s200299 <s200299@student.dhbw-mannheim.de>
2021-04-28 12:48:39 +0200

    Changes by natrocx

--- src/main/java/de/dhbw_mannheim/pfu_server/PfuServerApplication.java
+++ src/main/java/de/dhbw_mannheim/pfu_server/PfuServerApplication.java
@@ -2,6 +2,7 @@ package de.dhbw_mannheim.pfu_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PfuServerApplication {
