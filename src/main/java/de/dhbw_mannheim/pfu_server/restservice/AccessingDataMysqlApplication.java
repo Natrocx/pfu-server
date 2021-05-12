9f9c13264c0f1df818a97c697320da6bb128c951
Mahmoud <mahmodalmasry@hotmail.com>
2021-05-12 10:41:08 +0200

    Main controller erstellt

---
+++ src/main/java/de/dhbw_mannheim/pfu_server/restservice/AccessingDataMysqlApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw_mannheim.pfu_server.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataMysqlApplication.class, args);
+    }
+
+}
